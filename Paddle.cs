bdaa7c1808ec2eb90c0322dbba6101d1588940a8
Kacper Stefaniak <50682188+MisterCapi@users.noreply.github.com>
2022-05-03 19:17:17 +0200

    Update Paddle.cs

--- Paddle.cs
+++ Paddle.cs
@@ -34,10 +34,4 @@ public class Paddle : MonoBehaviour
 
         rb.velocity = new Vector2(rb.velocity.x, movement * speed);
     }
-
-    public void Reset()
-    {
-        rb.velocity = Vector2.zero;
-        transform.position = startPosition;
-    }
 }
